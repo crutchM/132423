518e6bedce1a608ec4d7158f63dd9eefe7b8bd05
crutchm <sads1100@gmail.com>
2024-01-26 19:07:28 +0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module 124123
+
+go 1.21.3
+
+require (
+	github.com/pkg/errors v0.9.1
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
+)
+
+require gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"gopkg.in/gomail.v2"
+	"log"
+)
+
+func main() {
+	adapter := NewSmtpAdapter("mail.hosting.reg.ru",
+		"ilyayaldinov@inbox.ru",
+		"RvSXC4wkVdunpmqkQE89",
+		"ilyayaldinov@inbox.ru", 587)
+
+	if err := adapter.Send("ilyayaldinov@inbox.ru", "ку", "ку"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type SmtpAdapter struct {
+	dialer *gomail.Dialer
+	email  string
+}
+
+func NewSmtpAdapter(host, login, password, email string, port int) *SmtpAdapter {
+	return &SmtpAdapter{
+		email:  email,
+		dialer: gomail.NewDialer(host, port, login, password),
+	}
+}
+
+func (smtpAdapter *SmtpAdapter) Send(target string, subject string, body string) error {
+	message := gomail.NewMessage()
+	message.SetHeader("From", smtpAdapter.email)
+	message.SetHeader("To", target)
+	message.SetHeader("Subject", subject)
+	message.SetBody("text/html", body)
+	return smtpAdapter.dialer.DialAndSend(message)
+}
+
+func (smtpAdapter *SmtpAdapter) MultipleSend(subject string, body string, targets ...string) error {
+	errList := make([]error, 0, 2)
+	for _, target := range targets {
+		if err := smtpAdapter.Send(target, subject, body); err != nil {
+			errList = append(errList, err)
+		}
+	}
+	return smtpAdapter.processErrors(errList)
+}
+
+func (smtpAdapter *SmtpAdapter) processErrors(errs []error) error {
+	errorMessage := "Ошибка отправки писем: "
+	if len(errs) == 0 {
+		return nil
+	}
+	for _, error := range errs {
+		errorMessage = fmt.Sprint(errorMessage, error.Error(), " ")
+	}
+
+	return errors.New(errorMessage)
+
+}
